f172e27f6828ad8101fb44c26f30edd8e0e4649d
pnwscm60 <46749603+pnwscm60@users.noreply.github.com>
2021-06-22 22:43:07 +0200

    Update config.php

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -3,21 +3,13 @@
  * Back end modules
  * Front end modules
  */
-use Pnwscm60\PbtimeBundle\Module\ModuleErfassen;
-use Pnwscm60\PbtimeBundle\Module\ModuleAdmin;
-use Pnwscm60\PbtimeBundle\Module\ModuleDaily;
-use Pnwscm60\PbtimeBundle\Module\ModuleProfil;
-use Pnwscm60\PbtimeBundle\Module\ModuleRReport;
-use Pnwscm60\PbtimeBundle\Module\ModuleReport;
-use Pnwscm60\PbtimeBundle\Module\ModuleVerkauf;
-use Pnwscm60\PbtimeBundle\Module\ModuleRegie;
+use Pnwscm60\BtoolsBundle\Module\ModuleErfassen;
+use Pnwscm60\BtoolsBundle\Module\ModuleAdmin;
+use Pnwscm60\BtoolsBundle\Module\ModuleProfil;
+use Pnwscm60\BtoolsBundle\Module\ModuleReport;
 $GLOBALS['FE_MOD']['pbtime'] = [ 
 	'erfassen' => ModuleErfassen::class,
 	'admin' => ModuleAdmin::class,
-	'daily' => ModuleDaily::class,
 	'profil' => ModuleProfil::class,
-	'regierap' => ModuleRReport::class,
-	'verkauf' => ModuleVerkauf::class,
-	'report' => ModuleReport::class,
-	'regie' => ModuleRegie::class
+	'report' => ModuleReport::class
 ];  
