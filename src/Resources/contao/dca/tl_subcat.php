6784bd816aeb0f730c07f83bdbd59473419983fd
pnwscm60 <46749603+pnwscm60@users.noreply.github.com>
2021-06-24 20:15:54 +0200

    Create tl_subcat.php

---
+++ src/Resources/contao/dca/tl_subcat.php
@@ -0,0 +1,137 @@
+<?php
+
+/**
+DCA für btools: tl_subcat
+© 2021 Markus Schenker, Phi Network
+ */
+
+
+/**
+ * Table tl_subcat
+ */
+$GLOBALS['TL_DCA']['tl_subcat'] = array
+(
+	// Config
+	'config' => array
+	(
+		'dataContainer'               => 'Table',
+		'enableVersioning'            => true,
+		'sql' => array
+		(
+			'keys' => array
+			(
+				'id' => 'primary'
+			)
+		),
+	),
+
+	// List
+	'list' => array
+	(
+		'sorting' => array
+		(
+			'mode'                    => 1,
+			'fields'                  => array('title'),
+			'flag'                    => 8,
+			'panelLayout'             => 'filter;sort,search,limit'
+		),
+		'label' => array
+		(
+			'fields'                  => array('title'),
+			'format'                  => '%s',
+		),
+		'global_operations' => array
+		(
+			'all' => array
+			(
+				'label'               => &$GLOBALS['TL_LANG']['MSC']['all'],
+				'href'                => 'act=select',
+				'class'               => 'header_edit_all',
+				'attributes'          => 'onclick="Backend.getScrollOffset()" accesskey="e"'
+			)
+		),
+		'operations' => array
+		(
+			'edit' => array
+			(
+				'label'               => &$GLOBALS['TL_LANG']['tl_subcat']['edit'],
+				'href'                => 'act=edit',
+				'icon'                => 'edit.gif'
+			),
+			'delete' => array
+			(
+				'label'               => &$GLOBALS['TL_LANG']['tl_subcat']['delete'],
+				'href'                => 'act=delete',
+				'icon'                => 'delete.gif',
+				'attributes'          => 'onclick="if(!confirm(\'' . $GLOBALS['TL_LANG']['MSC']['deleteConfirm'] . '\'))return false;Backend.getScrollOffset()"'
+			),
+			'show' => array
+			(
+				'label'               => &$GLOBALS['TL_LANG']['tl_subcat']['show'],
+				'href'                => 'act=show',
+				'icon'                => 'show.gif',
+				'attributes'          => 'style="margin-right:3px"'
+			),
+		)
+	),
+
+	// Palettes
+	'palettes' => array
+	(
+		'default'	=> 'title'
+	),
+
+	// Fields
+	'fields' => array
+	(
+		'id' => array
+		(
+			'sql'                     => "int(10) unsigned NOT NULL auto_increment"
+		),
+		'tstamp' => array
+		(
+			'sql'                     => "timestamp NOT NULL default CURRENT_TIMESTAMP"
+		),
+		// id_member
+		'memberid'     => array
+		(
+			'sql' => "int(10) unsigned NOT NULL default '0'"
+		),
+    'catid'     => array
+		(
+			'sql' => "int(10) unsigned NOT NULL default '0'"
+		),
+		'title' => array
+		(
+			'label'                   => &$GLOBALS['TL_LANG']['tl_category']['title'],
+			'inputType'               => 'text',
+			'exclude'                 => true,
+			'sorting'                 => true,
+			'flag'                    => 11,
+			'search'                  => true,
+			'eval'                    => array('mandatory'=>true, 'unique'=>false, 'maxlength'=>125, 'tl_class'=>'w50'),
+			'sql'                     => "varchar(125) NOT NULL default ''"
+		),
+		
+		'description' => array
+		(
+			'label'                   => &$GLOBALS['TL_LANG']['tl_category']['description'],
+			'inputType'               => 'text',
+			'exclude'                 => true,
+			'sorting'                 => true,
+			'flag'                    => 11,
+			'search'                  => true,
+			'eval'                    => array('mandatory'=>false, 'maxlength'=>255, 'tl_class'=>'w50'),
+			'sql'                     => "varchar(255) NOT NULL default ''"
+		),
+		// standard oder special
+		'typ'     => array
+		(
+			'sql' => "int(1) unsigned NOT NULL default '0'"
+		),
+		'active'     => array
+		(
+			'sql' => "int(1) unsigned NOT NULL default '0'"
+		),
+	)
+);
