85a21c1d3ca05df09ca3166167564cc53dcddf66
pnwscm60 <46749603+pnwscm60@users.noreply.github.com>
2021-06-25 09:51:18 +0200

    Update config.php

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -3,12 +3,14 @@
  * Back end modules
  * Front end modules
  */
-use Pnwscm60\BtoolsBundle\Module\ModuleErfassen;
+use Pnwscm60\BtoolsBundle\Module\ModuleEntry;
+use Pnwscm60\BtoolsBundle\Module\ModuleCategory;
 use Pnwscm60\BtoolsBundle\Module\ModuleAdmin;
 use Pnwscm60\BtoolsBundle\Module\ModuleProfil;
 use Pnwscm60\BtoolsBundle\Module\ModuleReport;
 $GLOBALS['FE_MOD']['pbtime'] = [ 
-	'erfassen' => ModuleErfassen::class,
+	'entry' => ModuleErfassen::class,
+	'category' => ModuleCategory::class,
 	'admin' => ModuleAdmin::class,
 	'profil' => ModuleProfil::class,
 	'report' => ModuleReport::class
